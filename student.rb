aca88a97348259173e8e45ada5b20bd921b2d0bf
Caren-Koroeny <carensiya5@gmail.com>
2023-05-16 10:34:44 +0300

    define an instance method play_hooky

--- student.rb
+++ student.rb
@@ -3,3 +3,7 @@ require_relative './person'
 class Student < Person
     attr_reader :classroom
     def initialize(_id, age, classroom, name = 'unknown' , parent_permission: true)
+        super(name, age, parent_permission: parent_permission)
+        @classroom = classroom
+      end
+      
