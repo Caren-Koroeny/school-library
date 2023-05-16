1a4b9fabdc143fd5d937ca20a29e34d1e6789dfc
Caren-Koroeny <carensiya5@gmail.com>
2023-05-16 12:43:34 +0300

    Removed id in student file

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(_id, age, classroom, name = 'unknown', parent_permission: true)
-    super(name, age, parent_permission: parent_permission)
+  def initialize( age, classroom, name = 'unknown', parent_permission: true)
+    super(nage, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
