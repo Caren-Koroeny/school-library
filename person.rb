43045e424228efefc688890addca3154e7ac9139
Caren-Koroeny <carensiya5@gmail.com>
2023-05-16 11:44:10 +0300

    fixed linters errors

--- person.rb
+++ person.rb
@@ -1,20 +1,20 @@
 class Person
-    def initialize(age,name = 'unknown', parent_permission: true)
-        @id = Random.rand(1..1000)
-        @age = age
-        @parent_permission = parent_permission
-        @name = name
-    end
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @parent_permission = parent_permission
+    @name = name
+  end
 
-    attr_accessor :name, :age
-    attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def of_age?
-        @age >= 18
-    end
-    private : of_age?
-    
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  def of_age?
+    @age >= 18
+  end
+  private :of_age?
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
--- student.rb
+++ student.rb
@@ -1,12 +1,14 @@
 require_relative './person'
 
 class Student < Person
-    attr_reader :classroom
-    def initialize(_id, age, classroom, name = 'unknown' , parent_permission: true)
-        super(name, age, parent_permission: parent_permission)
-        @classroom = classroom
-      end
-      def play_hooky
-        '¯(ツ)/¯'
-      end
-    end
+  attr_reader :classroom
+
+  def initialize(_id, age, classroom, name = 'unknown', parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
