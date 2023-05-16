d3b658c4fc66e36934785b9292178f3b66c32600
Caren-Koroeny <carensiya5@gmail.com>
2023-05-16 11:29:37 +0300

    Define a method can use services?

--- teacher.rb
+++ teacher.rb
@@ -6,3 +6,7 @@ class Teacher < Person
         super(id, name, age, parent_permission)
         @specialization = specialization
       end
+      def can_use_services?
+        true
+      end
+    end
