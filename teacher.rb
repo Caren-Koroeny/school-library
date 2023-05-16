2a12a10a7c5ba65c72bce1cae9d67b74f9b71a4d
Caren-Koroeny <carensiya5@gmail.com>
2023-05-16 11:37:55 +0300

    Fixed linters errors

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,13 @@
 require_relative './person'
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
-    def initialize(age, specialization, name = 'unknown', parent_permission: true)
-        super(id, name, age, parent_permission)
-        @specialization = specialization
-      end
-      def can_use_services?
-        true
-      end
-    end
+  def initialize(age, specialization, name = 'unknown', parent_permission: true)
+    super(id, name, age, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
