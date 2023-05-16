a117ae5ddbb8dfbc59a350eb485fede19591b7c2
Caren-Koroeny <carensiya5@gmail.com>
2023-05-16 11:25:50 +0300

    Initialize Class teacher

--- teacher.rb
+++ teacher.rb
@@ -1,3 +1,8 @@
 require_relative './person'
 class Teacher < Person
     attr_accessor :specialization
+
+    def initialize(age, specialization, name = 'unknown', parent_permission: true)
+        super(id, name, age, parent_permission)
+        @specialization = specialization
+      end
