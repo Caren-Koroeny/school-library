05f557690f4d2a312c206c474a49c762b6820619
Caren-Koroeny <carensiya5@gmail.com>
2023-05-16 13:56:58 +0300

    Fixed typo error

--- student.rb
+++ student.rb
@@ -4,7 +4,7 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(_age, classroom, name = 'unknown', parent_permission: true)
-    super(nage, name, parent_permission: parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
