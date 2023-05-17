cc7966d08eac38ba4c27163642cf66df4079a2bf
Caren-Koroeny <carensiya5@gmail.com>
2023-05-17 07:55:31 +0300

    Use super to inherit the method

--- person.rb
+++ person.rb
@@ -1,6 +1,7 @@
 require_relative './nameable'
 class Person
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @age = age
     @parent_permission = parent_permission
