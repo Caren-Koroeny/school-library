be28b4df3d833918355403b32596da5e38ceddec
Caren-Koroeny <carensiya5@gmail.com>
2023-05-17 16:32:42 +0300

    Inherit from nameable in person class

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 require_relative './decorators/nameable'
-class Person
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
