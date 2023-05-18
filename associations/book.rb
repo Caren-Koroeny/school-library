a084f80afe737c18ddcaa6ac2c625d17cd36a922
Caren-Koroeny <carensiya5@gmail.com>
2023-05-18 05:48:30 +0300

    define add rental method

--- associations/book.rb
+++ associations/book.rb
@@ -5,4 +5,11 @@ class Book
     def initialize(title, author)
         @title = title
         @author = author
+        @rentals = []
+    end
+  
+    def add_rental(rental)
+      @rentals.push(rental)
+      rental.book = self
+    end
       end
--- person.rb
+++ person.rb
@@ -9,10 +9,11 @@ class Person < Nameable
     @age = age
     @parent_permission = parent_permission
     @name = name
+    @rentals = []
   end
 
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id,  :rentals
 
   def of_age?
     @age >= 18
