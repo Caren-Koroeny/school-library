60f0f5c29c93b4ec7a9b1361614009f3635a9ba5
Caren-Koroeny <carensiya5@gmail.com>
2023-05-18 15:55:17 +0300

    Define app class and initialized all the instance variables needed

--- app.rb
+++ app.rb
@@ -4,3 +4,12 @@ require_relative './associations/rental'
 require_relative './person'
 require_relative './student'
 require_relative './teacher'
+
+class App
+    def initialize
+      @book = []
+      @person = []
+      @rental = []
+      @classroom = Classroom.new('grade 12')
+    end
+  
