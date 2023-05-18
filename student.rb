b622ac130c48c8144180c9c9e91cfb8ba1eb0e95
Caren-Koroeny <carensiya5@gmail.com>
2023-05-18 05:17:33 +0300

    adds the current instance (self) to the students array of the classroom object

--- student.rb
+++ student.rb
@@ -10,6 +10,8 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 
   def play_hooky
     '¯(ツ)/¯'
