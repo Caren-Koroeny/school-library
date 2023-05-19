3f4239a72c6a30e7eed7b33f493d344ec052a096
Caren-Koroeny <carensiya5@gmail.com>
2023-05-19 09:24:35 +0300

    implement show method options to users

--- main.rb
+++ main.rb
@@ -25,4 +25,19 @@ class Options
         end
         show_menu
       end
+      
+      def show_menu
+        puts "\nPlease choose an option by entering a number:"
+        puts '1 -  List all books'
+        puts '2 -  List all people'
+        puts '3 -  Create a person'
+        puts '4 -  Create a book'
+        puts '5 -  Create a rental'
+        puts '6 -  List all rentals for a given person id'
+        puts '7 -  Exit'
+        user_choice = gets.chomp
+        return exit if user_choice == '7'
+    
+        select_option(user_choice)
+      end
     
