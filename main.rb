3e2561166463d2a7239049c669a1c48fc33d0b5d
Caren-Koroeny <carensiya5@gmail.com>
2023-05-19 09:23:30 +0300

    Implementation of select_optionmethod using recursion

--- main.rb
+++ main.rb
@@ -6,4 +6,23 @@ class Options
         puts "\nWELCOME TO THE SCHOOL LIBRARY APP!\n"
         show_menu
     end
+    def select_option(user_choice)
+        case user_choice
+        when '1'
+          @app.list_all_book
+        when '2'
+          @app.list_all_people
+        when '3'
+          @app.create_person
+        when '4'
+          @app.create_book
+        when '5'
+          @app.create_rental
+        when '6'
+          @app.list_all_rentals
+        else
+          puts 'Invalid option, please try again!'
+        end
+        show_menu
+      end
     
